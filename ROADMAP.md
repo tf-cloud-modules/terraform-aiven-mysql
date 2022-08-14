a74305474d5d3ce33b85c2c5bc778db5742201e5
Andrei Lapkin <andrew.lapkin.writer@gmail.com>
2022-08-14 16:23:06 +0400

    feat(root): add module

---
+++ ROADMAP.md
@@ -0,0 +1,16 @@
+# Roadmap
+
+This document describes the current status of this project.
+
+
+| Status | Goal | Labels | 
+| :---: | :--- | --- | 
+| ✔ | [Set up basic module features]() || 
+| ❌ | [Add mysql_user_config]() |`in progress`|
+| ❌ | [Add mysql_user_config.migration]() |`in progress`|
+| ❌ | [Add mysql_user_config.mysql]() |`in progress`|
+| ❌ | [Add mysql_user_config.private_access]() |`in progress`|
+| ❌ | [Add mysql_user_config.privatelink_access]() |`in progress`|
+| ❌ | [Add mysql_user_config.public_access]() |`in progress`|
+| ❌ | [Add service_integrations]() |`in progress`|
+| ❌ | [Add examples]() |`in progress`|
---
+++ main.tf
@@ -0,0 +1,25 @@
+resource "aiven_mysql" "this" {
+  count                   = var.create ? 1 : 0
+  cloud_name              = var.cloud_name
+  maintenance_window_dow  = var.maintenance_window_dow
+  maintenance_window_time = var.maintenance_window_time
+  plan                    = var.plan
+  project                 = var.project
+  project_vpc_id          = var.project_vpc_id
+  static_ips              = var.static_ips
+  service_name            = var.service_name
+  termination_protection  = var.termination_protection
+
+  dynamic "tag" {
+    for_each = var.tags
+    content {
+      key   = lookup(service_integrations.value, "key", null)
+      value = lookup(service_integrations.value, "value", null)
+    }
+  }
+
+  lifecycle {
+    # We are ignoring a migration block to it's one-off nature.
+    ignore_changes = [mysql_user_config["migration"]]
+  }
+}
