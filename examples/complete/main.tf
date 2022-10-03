875116f730e3809dfef19c140693d99f3d9b889b
Andrei Lapkin <andrew.lapkin.writer@gmail.com>
2022-10-03 18:32:50 +0400

    chore(examples): apply one-variable approach

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,7 +1,7 @@
 module "mysql" {
   source       = "../.."
   project      = var.project
-  service_name = var.service_name
+  service_name = "test"
   tags = [
     {
       key   = "env"
@@ -14,12 +14,12 @@ module "database" {
   source        = "../../modules/database"
   project       = module.mysql.project
   service_name  = module.mysql.service_name
-  database_name = var.database_name
+  database_name = "test"
 }
 
 module "user" {
   source       = "../../modules/user"
   project      = module.mysql.project
   service_name = module.mysql.service_name
-  username     = var.username
+  username     = "test"
 }
